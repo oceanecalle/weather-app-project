c0694b910273154b4792d4eb6fb7b2402a8bb289
oceanecalle <143463859+oceanecalle@users.noreply.github.com>
2023-10-21 11:59:16 -0400

    Tried to change weather icon

--- src/index.js
+++ src/index.js
@@ -65,6 +65,11 @@ function showWeather(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
